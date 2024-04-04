0f8aa5a2c50c44255bd0e9ac7c477f34af809745
Singareddy Eswar Bhavishreddy <Bhavish.Reddy@xcdm.world>
2024-04-05 00:08:44 +0530

    1.2 courseinfo refactor Content component

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -1,15 +1,11 @@
+import Part from "./Part"
+
 const Content = (props) => {
   return (
     <div>
-      <p>
-        {props.data[0].name} {props.data[0].exercises}
-      </p>
-      <p>
-        {props.data[1].name} {props.data[1].exercises}
-      </p>
-      <p>
-        {props.data[2].name} {props.data[2].exercises}
-      </p>
+      <Part part={props.data[0]} />
+      <Part part={props.data[1]} />
+      <Part part={props.data[2]} />
     </div>
   )
 }
---
+++ part1/courseinfo/src/Part.jsx
@@ -0,0 +1,5 @@
+const Part = (props) => {
+    return <p>{props.part.name} {props.part.exercises}</p>
+}
+
+export default Part
